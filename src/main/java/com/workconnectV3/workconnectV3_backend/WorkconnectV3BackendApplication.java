45c1a681461d9d4ce639b6df9b459981f6d81d06
infra-dev <infracdo@gmail.com>
2025-01-09 16:56:51 +0800

    initial commit

---
+++ src/main/java/com/workconnectV3/workconnectV3_backend/WorkconnectV3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.workconnectV3.workconnectV3_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkconnectV3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkconnectV3BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workconnectV3/workconnectV3_backend/WorkconnectV3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workconnectV3.workconnectV3_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkconnectV3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
